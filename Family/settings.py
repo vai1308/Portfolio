d78e12a91ae937aff37daff7251de9fac7dd9aab
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2024-01-03 14:45:04 +0530

    Static Files Change

--- Family/settings.py
+++ Family/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_URL = '/images/'
 
 # Added Manually
